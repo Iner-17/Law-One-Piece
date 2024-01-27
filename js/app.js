58365003d957abafbbb0b2f4765d167b95d46ce7
Iner <inercollado52@gmail.com>
2024-01-27 22:34:42 +0800

    completed the first function [room effect]

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const roomEffect = document.getElementsByClassName('room-effect')[0];
+
+function room() {
+    roomEffect.style.width = "100%";
+    roomEffect.style.height = "100%";
+    roomEffect.style.borderRadius = '0';
+}
