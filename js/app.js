7826f0091ecd5cd5d21ac431b2cd1d443f35ae13
Iner <inercollado52@gmail.com>
2024-01-28 09:08:12 +0800

    fixed the sound issue

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 const roomEffect = document.getElementsByClassName('room-effect')[0];
 
-function room() {
-    roomEffect.style.width = "100%";
-    roomEffect.style.height = "100%";
-    roomEffect.style.borderRadius = '0';
-}
+
+    function room() {
+            roomEffect.style.width = "100%";
+            roomEffect.style.height = "100%";
+            roomEffect.style.borderRadius = '0';
+        
+            new Audio("/images/sound effect.mp3").play();
+
+
+            setTimeout(() => {
+                roomEffect.style.width = "0.01%";
+                roomEffect.style.height = "0.01%";
+                roomEffect.style.borderRadius = '50%';
+            }, 5000)
+    }
+
